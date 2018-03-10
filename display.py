be2de8113f3d53c3a309cd9447258622d97b29b7
sonadolasia <sonad8@gmail.com>
2018-03-09 19:02:46 -0800

    modified for display and added success

--- display.py
+++ display.py
@@ -17,15 +17,17 @@ class dexnetDisplay(QMainWindow):
 		central_widget = QWidget()
 		# Create some widgets
 		nameLabel = QLabel("Dex-Net Demo")
-		font = QFont("Helvetica Neue", 60, QFont.Bold)
-		smallFont = QFont("Helvetica Neue", 20, QFont.Bold)
+		font = QFont("Helvetica Neue", 95, QFont.Bold)
+		smallFont = QFont("Helvetica Neue", 33, QFont.Bold)
+		successFont = QFont("Helvetica Neue", 65, QFont.Bold)
 		#nameLabel.setAlignment(QtCore.Qt.AlignCenter)
 		leftBar = QProgressBar()
 		leftBar.setOrientation(QtCore.Qt.Vertical)
 		style ="""
 		        QProgressBar {
-		            border: 5px solid #000033;
-		            border-radius: 7px;
+		            border: 12px solid #000033;
+		            border-radius: 12px;
+		            width = 20px;
 		        }
 		        QProgressBar::chunk {
 		        	background-color: #ffcc00;
@@ -35,6 +37,10 @@ class dexnetDisplay(QMainWindow):
 		leftBar.setMaximum(10)
 		leftBar.setValue(5)
 		leftBar.setTextVisible(False)
+		#policy = QSizePolicy(QSizePolicy.IgnoreFlag, QSizePolicy.IgnoreFlag)
+		#leftBar.setSizePolicy(policy)
+		leftBar.setFixedWidth(100)
+
 		#leftBar.setFixedHeight(400)
 
 		rightBar = QProgressBar()
@@ -43,6 +49,7 @@ class dexnetDisplay(QMainWindow):
 		rightBar.setMaximum(10)
 		rightBar.setValue(3)
 		rightBar.setTextVisible(False)
+		rightBar.setFixedWidth(100)
 
 
 		pickBox = QVBoxLayout()
@@ -53,9 +60,9 @@ class dexnetDisplay(QMainWindow):
 		labelPicks = QLabel("PICKS PER HOUR")
 		labelPicks.setStyleSheet("color: white")
 		labelPicks.setFont(smallFont)
-		picksPerHour.setAlignment(Qt.AlignHCenter)
-		labelPicks.setAlignment(Qt.AlignHCenter)
-		backPick.setStyleSheet("border: 1px solid #000033; border-radius: 7px; background-color: #000033;")
+		picksPerHour.setAlignment(Qt.AlignCenter)
+		labelPicks.setAlignment(Qt.AlignCenter)
+		backPick.setStyleSheet("border: 1px solid #000033; border-radius: 12px; background-color: #000033;")
 		pickBox.addWidget(picksPerHour)
 		pickBox.addWidget(labelPicks)
 		backPick.setLayout(pickBox)
@@ -64,15 +71,15 @@ class dexnetDisplay(QMainWindow):
 
 		weightBox = QVBoxLayout()
 		backWeight = QWidget()
-		weightDisplay = QLabel("25.67")
+		weightDisplay = QLabel("25.6")
 		weightDisplay.setFont(font)
 		weightDisplay.setStyleSheet("border: 1px solid #000033; background-color: #000033; color: white")
 		labelWeight = QLabel("GRAMS")
 		labelWeight.setStyleSheet("color: white")
 		labelWeight.setFont(smallFont)
-		weightDisplay.setAlignment(Qt.AlignHCenter)
-		labelWeight.setAlignment(Qt.AlignHCenter)
-		backWeight.setStyleSheet("border: 1px solid #000033; border-radius: 7px; background-color: #000033;")
+		weightDisplay.setAlignment(Qt.AlignCenter)
+		labelWeight.setAlignment(Qt.AlignCenter)
+		backWeight.setStyleSheet("border: 1px solid #000033; border-radius: 12px; background-color: #000033;")
 		weightBox.addWidget(weightDisplay)
 		weightBox.addWidget(labelWeight)
 		backWeight.setLayout(weightBox)
@@ -86,12 +93,28 @@ class dexnetDisplay(QMainWindow):
 		labelConfidence = QLabel("% CONFIDENCE")
 		labelConfidence.setStyleSheet("color: white")
 		labelConfidence.setFont(smallFont)
-		confidenceDisplay.setAlignment(Qt.AlignHCenter)
-		labelConfidence.setAlignment(Qt.AlignHCenter)
-		backConfidence.setStyleSheet("border: 1px solid #000033; border-radius: 7px; background-color: #000033;")
-		confidenceBox.addWidget(confidenceDisplay)
-		confidenceBox.addWidget(labelConfidence)
+		confidenceDisplay.setAlignment(Qt.AlignCenter)
+		labelConfidence.setAlignment(Qt.AlignCenter)
+		backConfidence.setStyleSheet("border: 1px solid #000033; border-radius: 12px; background-color: #000033;")
+		confidenceBox.addWidget(confidenceDisplay, Qt.AlignCenter)
+		confidenceBox.addWidget(labelConfidence, Qt.AlignCenter)
 		backConfidence.setLayout(confidenceBox)
+
+		successBox = QVBoxLayout()
+		backSuccess = QWidget()
+		success = QLabel("SUCCESS")
+		success.setStyleSheet("border: 0px; color: #000033")
+		success.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+		success.setAlignment(Qt.AlignCenter)
+		backSuccess.setStyleSheet("border: 0px solid #000033; background-color: #ffcc00; border-radius: 12px")
+
+		failure = QLabel("FAILURE")
+		failure.setStyleSheet("color: white")
+		failure.setAlignment(Qt.AlignCenter)
+		failure.setFont(successFont)
+		success.setFont(successFont)
+		successBox.addWidget(success, Qt.AlignHCenter)
+		backSuccess.setLayout(successBox)
 
 
 
@@ -100,9 +123,16 @@ class dexnetDisplay(QMainWindow):
 		header.setPixmap(QPixmap(os.getcwd() + "/header.png"))
 		header.setScaledContents(True)
 
+		imageBox = QVBoxLayout()
+		backImage = QWidget()
 		image = QLabel()
 		image.setPixmap(QPixmap(os.getcwd() + "/camera1.png"))
+		#image.keepAspectRatio()
+		image.setStyleSheet("border: 10px; color: #000033; border-radius: 12px")
+		backImage.setStyleSheet("border: 3px solid #000033; background-color: #000033; border-radius: 8px")
 		image.setScaledContents(True)
+		imageBox.addWidget(image, Qt.AlignCenter)
+		backImage.setLayout(imageBox)
 
 		icons = QLabel()
 		icons.setPixmap(QPixmap(os.getcwd() + "/icons.png"))
@@ -110,33 +140,43 @@ class dexnetDisplay(QMainWindow):
 
 
 		sensing = QLabel()
-		sensing.setPixmap(QPixmap(os.getcwd() + "/sensing.png"))
+		sensing.setPixmap(QPixmap(os.getcwd() + "/all_off.png"))
 
 		pause = QLabel()
-		pause.setPixmap(QPixmap(os.getcwd() + "/pause_plain.png"))
+		pause.setPixmap(QPixmap(os.getcwd() + "/pause_action.png"))
 
 		error = QLabel()
-		error.setPixmap(QPixmap(os.getcwd() + "/error_off.png"))
+		error.setPixmap(QPixmap(os.getcwd() + "/error_on.png"))
 
 		top_layout = QGridLayout()
 
+		top_layout.setColumnStretch(0, 1)
+		top_layout.setColumnStretch(1, 2)
+		top_layout.setColumnStretch(2, 1)
+		top_layout.setRowStretch(0, 1.25)
+		top_layout.setRowStretch(1, 1.5)
+		top_layout.setRowStretch(2, 7)
+		top_layout.setRowStretch(3, 1)
+		top_layout.setRowStretch(4, 1)
+		top_layout.setRowStretch(5, 1)
 
-		top_layout.addWidget(leftBar, 1, 0, Qt.AlignHCenter)
-		top_layout.addWidget(rightBar, 1, 2, Qt.AlignHCenter)
-		top_layout.addWidget(backPick, 0, 0)
-		top_layout.addWidget(backWeight, 2, 0)
-		top_layout.addWidget(backConfidence, 0, 2)
+		top_layout.addWidget(leftBar, 2, 0, Qt.AlignHCenter)
+		top_layout.addWidget(rightBar, 2, 2, Qt.AlignHCenter)
+		top_layout.addWidget(backPick, 0, 2, 2, 1)
+		top_layout.addWidget(backWeight, 3, 0, 2, 1)
+		top_layout.addWidget(backConfidence, 3, 2, 2, 1)
+		top_layout.addWidget(backSuccess, 0, 0, 2, 1)
 		top_layout.addWidget(header, 0, 1, Qt.AlignCenter)
-		top_layout.addWidget(image, 1, 1, Qt.AlignCenter)
-		top_layout.addWidget(icons, 2, 1, Qt.AlignCenter)
-		top_layout.addWidget(sensing, 3, 1, Qt.AlignCenter)
-		top_layout.addWidget(pause, 3, 0, Qt.AlignCenter)
-		top_layout.addWidget(error, 3, 2, Qt.AlignCenter)
+		top_layout.addWidget(backImage, 1, 1, 3, 1, Qt.AlignCenter)
+		top_layout.addWidget(icons, 4, 1, Qt.AlignCenter)
+		top_layout.addWidget(sensing, 5, 1, Qt.AlignCenter)
+		top_layout.addWidget(pause, 5, 0, Qt.AlignCenter)
+		top_layout.addWidget(error, 5, 2, Qt.AlignCenter)
 
 		central_widget.setLayout(top_layout)
 		self.setCentralWidget(central_widget)
-		self.setFixedSize(1440, 810)
-		self.show()
+		self.setFixedSize(1920, 1080)
+		self.showFullScreen()
 
 
 if __name__ == "__main__":
